c290d37053e827938d5c8865653e8b80eb5c9392
kodinkata <kodinkata@users.noreply.github.com>
2020-10-13 11:00:09 -0700

    Create Heston_stochastic_volatility_model.r
    #USing Heston’s stochastic volatility model for the real world using an Euler scheme
    #Simulating 10,000 paths over 1 year horizon using given parameters
    #Output: plotting asset price paths and volatility paths

---
+++ Heston_stochastic_volatility_model.r
@@ -0,0 +1,64 @@
+#USing Heston's stochastic volatility model for the real world using an Euler scheme 
+#Simulating 10,000 paths over 1 year horizo using given parameters
+#Output: plotting asset price paths and volatility paths
+
+
+myHeston <- function(M,N,t,mu,alpha,b,volvol,S0,V0){
+  dt <- t/N
+  sqdt <- sqrt(dt)
+  S <- V <- matrix(NA,ncol=N+1,nrow=M)
+  S[,1] <- S0
+  V[,1] <- V0
+  for (i in 1:N){
+    Z1 <- matrix(rnorm(M),ncol=1)
+    Z2 <- matrix(rnorm(M),ncol=1)
+    S[,i+1] <- S[,i] + mu*S[,i]*dt +
+      sqrt(V[,i])*S[,i]*sqdt*Z1
+    V[,i+1] <- V[,i] + alpha*(b - V[,i])*dt +
+      volvol*sqrt(V[,i])*sqdt*Z2
+  }
+  out <- list("S"=S,"V"=V)
+  return(out)
+}
+# parameters:
+M <- 10000
+N <- 252
+t <- 1
+mu <- 0.1
+alpha <- 2
+b <- 0.4^2
+volvol <-0.3
+V0 <- 0.09
+S0 <- 100
+H <- myHeston(M,N,t,mu,alpha,b,volvol,S0,V0)
+time <- as.matrix(seq(0,t,length=N+1),ncol=1)
+S1 <- t(H$S)
+V1 <- t(H$V)
+df1 <- as.data.frame(cbind(time,S1))
+df2 <- as.data.frame(cbind(time,V1))
+# Plot some paths:
+mP <- 100 #limit the number of paths being plotted
+Anames <- rep("",mP)
+for (i in 1:mP){
+  Anames[i] <- paste("A",i,sep="")
+}
+names(df1) <- c("time", Anames)
+names(df2) <- c("time", Anames)
+
+library("reshape")
+library("ggplot2")
+df1.melted <- melt(df1[,c(1:mP)], id = "time")
+df2.melted <- melt(df2[,c(1:mP)], id = "time")
+plot.stocks <- ggplot() +
+  geom_line(data = df1.melted, aes(x = time, y = value, factor=variable),color="red") +
+  xlab('time') +
+  ylab('Stock Price')
+print(plot.stocks)
+plot.vol <- ggplot() +
+  geom_line(data = df2.melted, aes(x = time, y = value, factor=variable),color="black") +
+  xlab('time') +
+  ylab('Volatility')
+print(plot.vol)
+
+hist(H$S[,N+1])
+mean(H$S[,N+1])
